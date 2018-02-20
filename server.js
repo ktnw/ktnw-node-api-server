df298ab0a65d6fe3a481cd74be408eed17925b58
jakub <jakub.duha@email.cz>
2018-02-20 12:42:45 +0100

    Remove unneeded ejs dependency

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(methodOverride);
 
 // implement CORS from the frontend app
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:8000");
+  res.header("Access-Control-Allow-Origin", "http://localhost:8080");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   next();
@@ -36,7 +36,7 @@ app.use('*',function(req, res){
   res.status(404).send('Page Not Found!');
 });
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8000
 
 app.listen(PORT, function() {
   console.log('listening on ' + PORT)
